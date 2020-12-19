4dea0bfc3c6d1dc1cf97adda5d2de1f917c687ea
Matt White <matt@bitforge.us>
2020-12-18 23:56:30 -0700

    Newline

--- cec/client.go
+++ cec/client.go
@@ -169,7 +169,7 @@ func (l *Listener) Send(msg Message) {
 		values[index+2] = strconv.FormatInt(int64(each), 16)
 	}
 
-	command := "tx " + strings.ToUpper(strings.Join(values, ":"))
+	command := "tx " + strings.ToUpper(strings.Join(values, ":")+"\n")
 
 	fmt.Printf("CEC command sent: %s", command)
 
